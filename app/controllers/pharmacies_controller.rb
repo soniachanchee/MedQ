bf6b5e6344ae8b021edbf6c4f2d79db72ad04374
Nigel Fitzgerald <johnscottnigel@gmail.com>
2022-09-06 13:15:05 +0200

    started search feature, and pharmacies index and show

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,2 +1,17 @@
 class PharmaciesController < ApplicationController
+  def index
+    if params[:query].present?
+      @medicines = Medicine.where(name: params[:query])
+      # @medicines.stocks
+      # raise
+      @pharmacies = Pharmacy.all
+
+      @stocks = Stock.where(medicine_id: @medicines[0].id)
+      # raise
+    end
+  end
+
+  def show
+    @pharmacy = Pharmacy.find(params[:id])
+  end
 end
