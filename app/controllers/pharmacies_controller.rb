0f0eb2fcea8ab4fa7801f63756d0ff8d49732583
Nigel Fitzgerald <johnscottnigel@gmail.com>
2022-09-07 10:45:50 +0200

    added quantity option and search bar to pharmacies-show

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,5 +1,7 @@
 class PharmaciesController < ApplicationController
   def index
+    @query = params[:query]
+    session[:passed_variable] = @query
     if params[:query].present?
       @medicines = Medicine.where(name: params[:query])
       # @medicines.stocks
@@ -18,6 +20,21 @@ class PharmaciesController < ApplicationController
   end
 
   def show
+    @query = session[:passed_variable]
+    # @test = @query
+    # raise
     @pharmacy = Pharmacy.find(params[:id])
+    @stocks = Stock.where(pharmacy_id: @pharmacy.id)
+    @medicine = Medicine.where(name: @query)
+    # @medicines = Medicine.where()
   end
 end
+
+# def first
+#   @first_value = params[:f_name]
+#   session[:passed_variable] = @first_value
+# end
+
+# def second
+#   @first_value = session[:passed_variable]
+#   @get_value = @first_value
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,14 +26,27 @@ p pharmacy1
 doliprane = Medicine.new(name: "doliprane", prescription: false, price: 100)
 doliprane.save
 
+doliprane1000 = Medicine.new(name: "doliprane 1000mg", prescription: false, price: 250)
+doliprane1000.save
 p doliprane
 
 p doliprane.name
 
-stock_of_pharmacy1 = Stock.new(pharmacy_id: pharmacy1.id, medicine_id: doliprane.id, quantity: 4)
-stock_of_pharmacy1.save
+spasfon = Medicine.new(name: "spasfon", prescription: false, price: 300)
+spasfon.save
 
-stock_of_pharmacy2 = Stock.new(pharmacy_id: pharmacy2.id, medicine_id: doliprane.id, quantity: 4)
-stock_of_pharmacy2.save
+stock1_pharma1 = Stock.new(pharmacy_id: pharmacy1.id, medicine_id: doliprane.id, quantity: 4)
+stock1_pharma1.save
+
+stock2_pharma1 = Stock.new(pharmacy_id: pharmacy1.id, medicine_id: doliprane1000.id, quantity: 4)
+stock2_pharma1.save
+
+stock3_pharma1 = Stock.new(pharmacy_id: pharmacy1.id, medicine_id: spasfon.id, quantity: 9)
+stock3_pharma1.save
+
+stock1_pharma2 = Stock.new(pharmacy_id: pharmacy2.id, medicine_id: doliprane.id, quantity: 4)
+stock1_pharma2.save
+
+
 
 # p pharmacy1.stock
