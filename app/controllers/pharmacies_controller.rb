aab55ad6859a4f7275cc0b70ee4681f60f935050
Nigel Fitzgerald <johnscottnigel@gmail.com>
2022-09-08 11:30:53 +0200

    pharma show

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -5,14 +5,11 @@ class PharmaciesController < ApplicationController
 
     if params[:query].present?
       @medicines = Medicine.where(name: params[:query])
-      # @medicines.stocks
-      @pharmacies = Pharmacy.all
-
       if @medicines[0].present?
         @stocks = Stock.where(medicine_id: @medicines[0].id)
       end
-      # raise
     end
+
     respond_to do |format|
       format.html # Follow regular flow of Rails
       format.text { render partial: "pharmacies/list", locals: {medicines: @medicines}, formats: [:html] }
@@ -23,6 +20,7 @@ class PharmaciesController < ApplicationController
     @pharmacy = Pharmacy.find(params[:id])
     @query = session[:passed_variable]
     @medicine = Medicine.where(name: @query)
+
     @stock_med = Stock.where(medicine_id: @medicine)
     @stock_pharma = @stock_med.where(pharmacy_id: @pharmacy)
     # @stocks = Stock.where(pharmacy_id: @pharmacy.id)
