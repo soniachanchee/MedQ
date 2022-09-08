fb5b574b76ffcab017a277d6bad66ec5e04f3f36
Nigel Fitzgerald <johnscottnigel@gmail.com>
2022-09-08 11:56:03 +0200

    validations

--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -2,7 +2,7 @@ class Pharmacy < ApplicationRecord
 
   has_many :stocks
   has_many :medicines, through: :stock
-  has_many :orders
+  has_many :orders, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
